a70ec8751b1c24541a89500a70e617fc03dc7266
spoirier271 <spoirier@ucsc.edu>
2016-05-12 13:54:11 -0700

    Update db_my_gpio.h

--- db_my_gpio.h
+++ db_my_gpio.h
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "db_feedback.h"
-
 #define WORK_PIN 7
+#define ALARM_PIN 0
 
 void gpio_ready();
 void pin_init();
 void map(int *);
 int reverse_map(int);
 double write_to_pin(int, double);
+void write_to_alarm_pin(int, double);
 void turn_off_all_pins();
