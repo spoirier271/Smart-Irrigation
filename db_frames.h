f17bb65ee521e86b5d65c70c73b03f5afd47c283
spoirier271 <spoirier@ucsc.edu>
2016-05-12 13:52:12 -0700

    Update db_frames.h

--- db_frames.h
+++ db_frames.h
@@ -4,18 +4,22 @@
 #include <stdbool.h>
 #include "sqlite3.h"
 
-#define FRAMES_TO_GET 96
-#define NUM_OF_MOISTURE_SENSORS 3
-#define DATABASE_NAME "../irrigation_data.db"
-#define TABLE_NAME "entry"
-#define TOTAL_COLUMNS 8
+#define FRAMES_TO_GET 4
+#define MAX_DRY_FRAMES FRAMES_TO_GET * 0.5
+
+#define NUM_OF_MOISTURE_SENSORS 2
+#define NUM_OF_TEMP_SENSORS 3
+#define DATABASE_NAME "../irrigation_data2.db"
+#define TABLE_NAME "entry2"
+#define TOTAL_COLUMNS 9
 
 #define DELIM "\t"
 
 struct data {
 	int moisture_sensor_value[NUM_OF_MOISTURE_SENSORS];
 	int moisture_sensor_average_value;
-	int temperature_sensor_value;
+	int temperature_sensor_value[NUM_OF_TEMP_SENSORS];
+	int temperature_sensor_average_value;
 };
 
 struct frame {
@@ -24,15 +28,18 @@ struct frame {
 	char time[100];
 	char date[100];
 	char julian[100];
+	bool bone_dry;
 };
 
 static const struct frame empty_frame;
 
 void frame_init(struct frame frame);
 struct frame get_frame(char *, int, struct frame*);
+int check_bone_dry(struct frame[], int);
 int line_check(char * );
 struct frame get_average(struct frame[], int);
 int moisture_average(int[], int);
+int temperature_average(int[], int);
 void print_frame(int, struct frame[]);
 
 void print_all_database_entries(char *, char *);
